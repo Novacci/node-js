142d3b6d553b593abc87cbe6919d0f0da8ec00d8
Wojciech Nowak <wojtekn00@gmail.com>
2023-08-27 23:34:38 +0200

    Streams / all solutions

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -4,7 +4,6 @@ const server = require('http').createServer();
 
 server.on('request', (req, res) => {
   // Solution 1
-
   // fs.readFile('dog.txt', (err, data) => {
   //   if (err) {
   //     console.log(err);
@@ -12,16 +11,25 @@ server.on('request', (req, res) => {
   //     res.end(data);
   //   }
   // });
-
   // Solution 2: Streams
-  const readableStream = fs.createReadStream('dog.txt');
-  readableStream.on('data', (pieceofData) => {
-    res.write(pieceofData);
-  });
+  // const readableStream = fs.createReadStream('dog.txt');
+  // readableStream.on('data', (pieceofData) => {
+  //   res.write(pieceofData);
+  // });
+  // readableStream.on('end', () => {
+  //   res.end();
+  // });
+  // readableStream.on('error', (err) => {
+  //   console.log(err);
+  //   res.statusCode = 500;
+  //   res.end('File not found');
+  // });
+  // Solution 3 - Pipe operator
 
-  readableStream.on('end', () => {
-    res.end();
-  });
+  const readableStream = fs.createReadStream('dog.txt');
+  readableStream.pipe(res);
+
+  //! readableSource.pipe(writeableDest)
 });
 
 server.listen(8000, '127.0.0.1', () => {
