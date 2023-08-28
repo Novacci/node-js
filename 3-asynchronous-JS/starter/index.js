d0c71c9c90931352dfd6a3853b4cc686c4ece0b4
Wojciech Nowak <wojtekn00@gmail.com>
2023-08-28 19:16:39 +0200

    callback hell in node.js reminder

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,37 +1,14 @@
 const fs = require('fs');
+const superagent = require('superagent');
 
-const server = require('http').createServer();
-
-server.on('request', (req, res) => {
-  // Solution 1
-  // fs.readFile('dog.txt', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     res.end(data);
-  //   }
-  // });
-  // Solution 2: Streams
-  // const readableStream = fs.createReadStream('dog.txt');
-  // readableStream.on('data', (pieceofData) => {
-  //   res.write(pieceofData);
-  // });
-  // readableStream.on('end', () => {
-  //   res.end();
-  // });
-  // readableStream.on('error', (err) => {
-  //   console.log(err);
-  //   res.statusCode = 500;
-  //   res.end('File not found');
-  // });
-  // Solution 3 - Pipe operator
-
-  const readableStream = fs.createReadStream('dog.txt');
-  readableStream.pipe(res);
-
-  //! readableSource.pipe(writeableDest)
-});
-
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening...');
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      console.log(res.body.message);
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        console.log('Random dog saved into file!');
+      });
+    });
 });
