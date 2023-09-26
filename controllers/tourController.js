523b53a895f85e68cb68ee428723404d7e91170b
Wojciech Nowak <wojtekn00@gmail.com>
2023-09-26 23:05:23 +0200

    Aggregation pipeline -> unwinding and projecting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -141,7 +141,34 @@ exports.getTourStats = async (req, res) => {
 exports.getMonthlyPlan = async (req, res) => {
   try {
     const year = +req.params.year;
-    const plan = await Tour.aggregate([]);
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $month: '$startDates',
+          },
+          numTourStars: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      { $addFields: { month: '$_id' } },
+      { $project: { _id: 0 } },
+      { $sort: { numTourStars: -1 } },
+      {
+        $limit: 6,
+      },
+    ]);
     res.status(200).json({
       status: 'succes',
       data: {
