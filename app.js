a7e334317f56c7192e6d96440c584441c6c73d5b
Wojciech Nowak <wojtekn00@gmail.com>
2023-10-10 18:52:32 +0200

    handled error for route that donesnt exist

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ app.use(express.json());
 app.use(express.static(`./public`));
 
 app.use((req, res, next) => {
-  console.log('Hey - Middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -29,6 +24,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter); //! middleware
 app.use('/api/v1/users', userRouter); //! middleware
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 //! Start server
 
