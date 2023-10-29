8565a666e38efc37507debee72f533a21481e928
Wojciech Nowak <wojtekn00@gmail.com>
2023-10-29 23:59:42 +0100

    Added reset token for reset password functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,6 +18,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
     role: req.body.role,
+    resetToken: req.body.resetToken,
   });
   const token = signToken(newUser._id);
 
@@ -101,3 +102,18 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  //! 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new AppError('There is no user with that email address', 404));
+  }
+  //! 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  //! 3) Send it to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
