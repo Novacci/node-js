4f8f0e22ce7e8539311b16b3a9505d84cae48a6c
Wojciech Nowak <wojtekn00@gmail.com>
2023-11-03 16:07:10 +0100

    Implemented helmet for security headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const AppError = require('./utils/appError');
@@ -20,8 +21,14 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
-
-app.use(express.json());
+// security http headers
+app.use(helmet());
+// body parser
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
 app.use(express.static(`./public`));
 
 app.use((req, res, next) => {
